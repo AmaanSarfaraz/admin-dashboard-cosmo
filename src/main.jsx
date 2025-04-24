c0a456f7cbb6414018761d5cb5c35551620ab510
AMAAN SARFARAZ <sarfarazmohammad369@gmail.com>
2025-04-24 16:11:48 +0530

    login component done

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import App from "./App.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
